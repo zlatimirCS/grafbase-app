3161dd8afe8879d2591842c6de9da32e2286a916
zlatimirCS <zlatimir_rk@yahoo.com>
2023-11-03 19:54:07 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <section className="flex-start flex-col paddings mb-16">
+        <h1>Categories</h1>
+        <h1>Posts</h1>
+        <h1>LoadMore</h1>
+      </section>
+    </>
+  );
+};
+
+export default Home;
