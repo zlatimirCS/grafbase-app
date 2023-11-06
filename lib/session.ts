b2a0913bf2b8448e3771504afb8344ba7e1ac733
zlatimirCS <zlatimir_rk@yahoo.com>
2023-11-06 13:21:01 +0100

    auth providers continued

---
+++ lib/session.ts
@@ -0,0 +1,34 @@
+import { getServerSession } from "next-auth/next";
+import { NextAuthOptions, User } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import jsonwebtoken from "jsonwebtoken";
+import { JWT } from "next-auth/jwt";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  // jwt: {
+  //   encode: ({ secret, token }) => {},
+  //   decode: async ({ secret, token }) => {},
+  // },
+  callbacks: {
+    async session({ session }) {
+      return session;
+    },
+    async signIn({ user }: { user: AdapterUser | User }) {
+      try {
+        // get the user if they exist
+        // if they don't exist, create them
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+  },
+};
