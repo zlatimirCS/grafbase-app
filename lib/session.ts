c8043bb0aff6b5ed5bec14342c95da6dbc9c639e
zlatimirCS <zlatimir_rk@yahoo.com>
2023-11-07 12:16:37 +0100

    add into grafbase main config

--- lib/session.ts
+++ lib/session.ts
@@ -4,6 +4,8 @@ import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
 import jsonwebtoken from "jsonwebtoken";
 import { JWT } from "next-auth/jwt";
+import { SessionInterface, UserProfile } from "@/common.types";
+import { getUser, createUser } from "./actions";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -18,12 +20,36 @@ export const authOptions: NextAuthOptions = {
   // },
   callbacks: {
     async session({ session }) {
-      return session;
+      const email = (session.user?.email as string) || "";
+      try {
+        const data = (await getUser(email)) as { user: UserProfile };
+        const newSession = {
+          ...session,
+          user: {
+            ...session.user,
+            ...data.user,
+          },
+        };
+        return newSession;
+      } catch (error) {
+        console.error(error);
+        return session;
+      }
     },
     async signIn({ user }: { user: AdapterUser | User }) {
       try {
         // get the user if they exist
         // if they don't exist, create them
+        const userExist = (await getUser(user?.email as string)) as {
+          user?: UserProfile;
+        };
+        if (!userExist.user) {
+          await createUser(
+            user?.name as string,
+            user?.email as string,
+            user?.image as string
+          );
+        }
         return true;
       } catch (error) {
         console.error(error);
@@ -32,3 +58,9 @@ export const authOptions: NextAuthOptions = {
     },
   },
 };
+
+export async function getCurrentUser() {
+  const session = (await getServerSession(authOptions)) as SessionInterface;
+
+  return session;
+}
